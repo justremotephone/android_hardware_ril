b9a4154660d118ab306b07f511ac13026b057acc
flintman <flintman@flintmancomputers.com>
2014-06-14 18:39:13 -0400

    ril;  Fix up mismerge
    Change-Id: I5d8db628106c97fca1cc39b43946115068aa7541

--- include/telephony/ril.h
+++ include/telephony/ril.h
@@ -3630,30 +3630,6 @@ typedef struct {
 #define RIL_REQUEST_GET_DATA_CALL_PROFILE 114
 
 
-/**
- * RIL_REQUEST_GET_DATA_CALL_PROFILE
- *
- * Get the Data Call Profile for a particular app type
- *
- * "data" is const int*
- * (const int*)data[0] - App type. Value is specified the RUIM spec C.S0023-D
- *
- *
- * "response" is a const char * containing the count and the array of profiles
- * ((const int *)response)[0] Number RIL_DataCallProfileInfo structs(count)
- * ((const char *)response)[1] is the buffer that contains 'count' number of
- *                              RIL_DataCallProfileInfo structs.
- *
- * Valid errors:
- *  SUCCESS
- *  GENERIC_FAILURE
- *  RIL_E_DATA_CALL_PROFILE_ERROR
- *  RIL_E_DATA_CALL_PROFILE_NOT_AVAILABLE
- *
- */
-#define RIL_REQUEST_GET_DATA_CALL_PROFILE 113
-
-
 /***********************************************************************/
 
 
